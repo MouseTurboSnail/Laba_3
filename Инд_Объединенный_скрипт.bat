b76ffb2ae421098f4438dd85c54cb9f171629ba9
MouseTurboSnail <cnikita98@mail.ru>
2020-04-15 16:15:12 +0300

    firstcommit

---
+++ Инд_Объединенный_скрипт.bat
@@ -0,0 +1,28 @@
+rem create new catalog
+md Nikitin
+rem go to into new catalog
+cd Nikitin
+rem create new catalog
+md Nikita
+rem go to into new catalog
+cd Nikita
+md Sergeevich
+rem create new catalog
+pause
+cd Sergeevich
+echo > 659956.txt
+cd ..
+cd ..
+echo > 22091998.txt
+cd ..
+pause
+del Nikitin /S /Q /F
+pause
+cd Nikitin
+cd Nikita
+rd Sergeevich
+cd ..
+rd Nikita
+cd ..
+rd Nikitin
+pause
